ce87843c9caab761a770110df340cb4c0a1a7c83
Florents Tselai <florents.tselai@gmail.com>
2024-06-23 20:19:28 +0300

    Formatting

--- tests/conftest.py
+++ tests/conftest.py
@@ -30,10 +30,7 @@ def fresh_db_path(db_path):
 @pytest.fixture
 def existing_db_path(fresh_db_path):
     db = Database(fresh_db_path)
-    table = db.create_table(
-        "prompts",
-        {"prompt": str, "generated": str}
-    )
+    table = db.create_table("prompts", {"prompt": str, "generated": str})
 
     table.insert({"prompt": "hello world!"})
     table.insert({"prompt": "how are you?"})
